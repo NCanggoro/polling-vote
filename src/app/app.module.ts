f4b99740d61f58c7c0231fd3029967619ea8e680
NCanggoro <figuranajah@gmail.com>
2018-05-12 12:41:04 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { createCustomElement} from '@angular/elements';
 import { AppComponent } from './app.component';
 import { FrameworkPollComponent } from './framework-poll/framework-poll.component';
 
+//copy config from firebase 
 const config = {
-  apiKey: "AIzaSyAvEgQmxAeUKNhteEgvUTE7aL5vd9uBYns",
-  authDomain: "framework-poll-fb11d.firebaseapp.com",
-  databaseURL: "https://framework-poll-fb11d.firebaseio.com",
+  apiKey: "PUT YOUR API KEY HERE",
+  authDomain: "api domain",
+  databaseURL: "datanse url",
   projectId: "framework-poll-fb11d",
   storageBucket: "framework-poll-fb11d.appspot.com",
   messagingSenderId: "1075619020166"
